fa400fa19357a73251ab2ae44d9cc0b97976302b
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-10-21 01:03:03 +0530

    fix: update js imports for marker & popup

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import GeolocateControl from './components/UI/controls/GeolocateControl';
 import FullscreenControl from './components/UI/controls/FullscreenControl';
 import AttributionControl from './components/UI/controls/AttributionControl';
 import ScaleControl from './components/UI/controls/ScaleControl';
-import Marker from './components/UI/Marker.vue';
-import Popup from './components/UI/Popup.vue';
+import Marker from './components/UI/Marker';
+import Popup from './components/UI/Popup';
 
 import GeojsonLayer from './components/layer/GeojsonLayer';
 import ImageLayer from './components/layer/ImageLayer';
@@ -49,5 +49,3 @@ export const MglRasterLayer = RasterLayer;
 
 export const MglMarker = Marker;
 export const MglPopup = Popup;
-
-export default GlMap;
